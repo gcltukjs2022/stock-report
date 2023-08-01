9d479d76376229899f0944cc038e5b15ef4b6840
georgechungOpop <george.chung@opopmedia.co.uk>
2023-08-01 18:30:45 +0100

    rename

--- src/components/StocksSelection/index.tsx
+++ src/components/StocksSelection/index.tsx
@@ -9,7 +9,7 @@ import {
   List,
 } from "./styles";
 import { useEffect, useState } from "react";
-import { yahooApi } from "../../utils/yahooApi";
+import { getReport } from "../../utils/getReport";
 import { data } from "./data";
 import { Loading } from "notiflix";
 import {
@@ -17,8 +17,11 @@ import {
   updateStocksPrices,
 } from "../../redux/features/stocksPricesSlice";
 import { useAppDispatch } from "../../redux/hooks";
+import moment from "moment";
 
 const StocksSelection = () => {
+  const [fileUrl, setFileUrl] = useState("");
+  const [ready, setReady] = useState(false);
   const stocks = data;
   const dispatch = useAppDispatch();
   // const [stockData, setStockData] = useState<StockPrice[]>();
@@ -27,22 +30,48 @@ const StocksSelection = () => {
 
   const handleClick = async () => {
     try {
-      // Loading.dots();
-      const res: any = await yahooApi();
-      if (res.data.success) {
-        setStockData(res.data.data.priceResult);
-        dispatch(updateStocksPrices(res.data.data.priceResult));
-        setShowPrice(true);
-        // Loading.remove();
+      Loading.circle();
+      const response: any = await getReport();
+      Loading.remove();
+      if (response.data.success) {
+        const base64Data = response.data.file;
+        const dataUri = `data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,${base64Data}`;
+        setFileUrl(dataUri);
+        setReady(true);
       }
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      Loading.remove();
+      console.error("Error:", error);
     }
+  };
+
+  const handleDownload = () => {
+    // Create a temporary link element
+    const link = document.createElement("a");
+    link.href = fileUrl;
+
+    // Extract the file name from the data URI
+    const fileName = `report${moment(new Date()).format("DDMMYYYY")}.docx`; // Replace "your_custom_file_name" with the desired file name
+    link.download = fileName;
+
+    // Append the link to the document and trigger the click event
+    document.body.appendChild(link);
+    link.click();
+
+    // Remove the temporary link element
+    document.body.removeChild(link);
   };
 
   return (
     <Container>
       <Button onClick={handleClick}>Refresh</Button>
+      {ready && (
+        <div>
+          <h2>Your file is ready</h2>
+          <div onClick={handleDownload}>Click here to download</div>
+        </div>
+      )}
+
       <List>
         {/* <Company>
           <Item>Project</Item>
@@ -50,7 +79,7 @@ const StocksSelection = () => {
           <Item>Listing Ticker</Item>
           <Item>Local CCY</Item>
         </Company> */}
-        {showPrice &&
+        {/* {showPrice &&
           stockData!.map((stock: any, index: number) => {
             return (
               <ItemWrapper key={index}>
@@ -70,7 +99,7 @@ const StocksSelection = () => {
                 </Details>
               </ItemWrapper>
             );
-          })}
+          })} */}
       </List>
     </Container>
   );
--- src/utils/getReport.ts
+++ src/utils/getReport.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { iData } from "../components/StocksSelection/data";
 
-export const yahooApi = async () => {
+export const getReport = async () => {
   try {
-    const url = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
-    const result = await axios.get(url + "/stock-price");
+    const url =
+      process.env.LIVE_REACT_APP_BACKEND_URL || "http://localhost:3001";
+    const result = await axios.get(url + "/stock-report");
 
     console.log(result);
     return result;
